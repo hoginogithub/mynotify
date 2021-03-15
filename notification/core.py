657b75028d1eae6cc737723f595326a8d1332919
hoginogithub <hogino@h7.dion.ne.jp>
2021-03-15 10:23:47 +0900

    bugfix mynotify timeout arguments

--- notification/core.py
+++ notification/core.py
@@ -11,32 +11,32 @@ import time
 def cli(t2150, t2200, timeout, title, msg):
     mynotify(t2150, t2200, timeout, title, msg)
 
-def notify_2150(in_timeout=60):
+def notify_2150(timeout=60):
     notification.notify(
         title = "<<!!! PC終了予告 !!!>>",
         message = "生活リズムを安定化させるために、22:00にはPC作業を終了しましょう",
-        timeout = in_timeout
+        timeout = timeout
     )
 
-def notify_2200(in_timeout=60):
+def notify_2200(timeout=60):
     notification.notify(
         title = "<<!!! PC終了勧告 !!!>>",
         message = "22:00です。10分後にPCは強制終了します。\n作業を終了してください。",
-        timeout = in_timeout
+        timeout = timeout
     )
 
-def notify_msg(in_title="", in_msg="", in_timeout=60):
+def notify_msg(title="", msg="", timeout=60):
     notification.notify(
-        title = in_title,
-        message = in_msg,
-        timeout = in_timeout
+        title = title,
+        message = msg,
+        timeout = timeout
     )
 
 def mynotify(t2150=False, t2200=False, timeout=10, title='(no title)', msg='(no message)'):
 
     if t2150:
-        notify_2150(in_timeout=timeout)
+        notify_2150(timeout=timeout)
     elif t2200:
-        notify_2200(in_timeout=timeout)
+        notify_2200(timeout=timeout)
     else:
-        notify_msg(in_title=title, in_msg=msg, in_timeout=timeout)
+        notify_msg(title=title, msg=msg, timeout=timeout)
