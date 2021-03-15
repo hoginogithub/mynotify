8f0325e3a3afa55fecb96981165f9ee0dcb7533f
hoginogithub <hogino@h7.dion.ne.jp>
2021-03-15 10:58:34 +0900

    fix: setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,16 @@ from os import name
 from setuptools import setup, find_packages
 
 setup(
-    name = 'notification',
+    name = 'mynotify',
     version = '1.0.0',
-    packages = find_packages(),
+    packages = find_packages(exclude=("test",)),
+    install_requires=[
+        'Click~=7.1',
+        'plyer~=2.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'mynotify=mynotify.core:cli'
+        ]
+    }
 )
